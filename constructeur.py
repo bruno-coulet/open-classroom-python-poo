4a5af02c4e8cae518480beba00360f2aaaf9aed0
Bruno Coulet <bruno.coulet@laplateforme.io>
2024-01-14 11:03:15 +0100

    ajout de consructeur.py

---
+++ constructeur.py
@@ -0,0 +1,41 @@
+class ToolBox:
+
+    def __init__(self):
+        self.tool = []
+        """Initialise les outils."""
+ 
+    def add_tool(self, tool):
+        pass
+
+    def remove_tool(self, tool):
+        pass
+
+
+
+class Screwdriver:
+
+    def __init__(self):
+        """Initialise la taille."""
+        self.size = size
+
+    def tighten(self, screw):
+        pass
+    def loosen(self, screw):
+        pass
+
+class Marteaux:
+    color = "red"
+
+    def __init__(self, color="red"):
+        """Initialise la couleur."""
+        self.color = color
+
+    def paint(self, color):
+        """Paint le marteau."""
+        pass
+
+    def hammerin(self, nail):
+        """Enfonce un clou."""
+        pass
+    def remove(self, nail):
+        pass   
